95d37410d4c7a3200c0557a5e866d662af5f52c6
Ranvir Prasad <ranvir.prasad@gmail.com>
2019-07-13 23:50:11 +0530

    hi

--- Module.php
+++ Module.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace app\modules\users;
+namespace rp\users;
 
 class Module extends \yii\base\Module
 {
-    public $controllerNamespace = 'app\modules\users\controllers';
+    public $controllerNamespace = 'rp\users\controllers';
 
     public function init()
     {
         parent::init();
-        
+
         // custom initialization code goes here
     }
 }
